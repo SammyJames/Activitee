c148f133853cdf132243c9a895a90dc1434e2e6a
Pawkette <pawkette.heals@gmail.com>
2013-09-15 19:51:54 -0700

    Initial Commit

---
+++ Activitee.lua
@@ -0,0 +1,352 @@
+--
+-- @Author: Pawkette
+-- @Date: 9/14/13
+--
+require "table"
+require "lib/lib_Debug"
+require "lib/lib_Vector"
+require "./ActivityEntry"
+
+local skMainFrameId         = "Main"
+local skActivitiesId        = "Activities"
+local skActivityMainGroupId = "ActivityMainGroup"
+local skGoalIconId          = "GoalIcon"
+local skGoalLabelId         = "GoalLabel"
+local skGoalMainGroupId     = "GoalMainGroup"
+local skGoalDetailGroupId   = "GoalDetailGroup"
+local skShoppingItemNameId  = "ShoppingItemName"
+local skGoalListId          = "GoalList"
+local skDistanceUpdateRate  = 0.1
+local skMinDistance         = 5
+local skMaxDistance         = 10
+
+local kSinView
+local kShoppingItemName
+local kGoalDetailGroup
+local kGoalIcon
+local kGoalList
+local kGoalMainGroup
+local kGoalLabel
+local kComponentFrame
+local kActivityMainGroup
+local kActivitiesWidget
+local kShoppingList
+local kActivityData         = {}
+local kActivityDisplay      = {}
+local fnDistanceCheck
+local kGoalWidth
+local kPlayerPosition       = { x = 0, y = 0, z = 0 }
+
+function Initialize()
+    Debug.EnableLogging( true )
+    kComponentFrame         = Component.GetFrame( skMainFrameId )
+    kActivityMainGroup      = Component.GetWidget( skActivityMainGroupId )
+    kActivitiesWidget       = Component.GetWidget( skActivitiesId )
+
+    kGoalMainGroup          = Component.GetWidget( skGoalMainGroupId )
+    kGoalIcon               = Component.GetWidget( skGoalIconId )
+    kGoalLabel              = Component.GetWidget( skGoalLabelId )
+
+    kGoalDetailGroup        = Component.GetWidget( skGoalDetailGroupId )
+    kGoalList               = Component.GetWidget( skGoalListId )
+    kShoppingItemName       = Component.GetWidget( skShoppingItemNameId )
+end
+
+--- Unique ID for actitivies
+-- Combines the typename of the activity with the id
+-- @param pActivity
+--
+function UID( pActivity )
+    return ( pActivity.typeName .. "#" .. tostring( pActivity.id ) )
+end
+
+--- Determines if an activity has no objectives
+-- @param pActivity
+--
+function NoObjectives( pActivity )
+    return ( ( not pActivity.objectives ) or ( #pActivity.objectives == 0 ) )
+end
+
+--- Determines if an activity is already active
+-- @param pActivity
+--
+function AlreadyActive( pActivity )
+    local result = false
+    for _,objective in ipairs( pActivity.objectives ) do
+        result = ActiveObjective( objective )
+        if result then break end
+    end
+
+    return result
+end
+
+--- Determines if an objective is active
+-- @param pObjective
+--
+function ActiveObjective( pObjective )
+    return ( ( pObjective.description ) and ( pObjective.description ~= "" )
+            and ( pObjective.active ) and ( not pObjective.completed ) )
+end
+
+--- Remove an activity
+-- really just does what the function says
+-- @param pActivity
+--
+function RemoveActivity( pActivity )
+    local key = UID( pActivity )
+
+    if ( kActivityDisplay[ key ] ) then
+        Component.RemoveWidget( kActivityDisplay[ key ].mGroup )
+        kActivityDisplay[ key ] = nil
+    end
+
+    kActivityData[ key ] = nil
+    if ( kActivitiesWidget:GetChildCount() == 0 ) then
+        kActivityMainGroup:Hide()
+    end
+
+    RearrangeActivites()
+end
+
+--- Add or Updates an activity
+-- if no entry exists, will create a new one or update existing
+-- @param pActivity
+--
+function UpdateActivity( pActivity )
+    local key = UID( pActivity )
+    kActivityData[ key ] = pActivity
+
+    if ( NoObjectives( pActivity ) or AlreadyActive( pActivity ) ) then
+        local activity_display = kActivityDisplay[ key ]
+
+        if ( not activity_display ) then
+            activity_display = ActivityEntry( pActivity, key, kActivitiesWidget )
+            kActivityDisplay[ key ] = activity_display
+        else
+            activity_display:RemoveAllMarkers()
+            activity_display:RemoveAllObjectives()
+        end
+
+        for _,objective in ipairs( pActivity.objectives ) do
+            local active = ActiveObjective( objective )
+
+            if ( active ) then
+                local obj = activity_display:CreateObjective( objective )
+                local count = activity_display:NumObjectives()
+                local height = obj.mWidget:GetBounds().height
+
+                obj.mWidget:SetDims( "height:_; center-y:" .. count * height * 2 )
+
+                activity_display:SetFullHeight( ( count * height * 2 ) + 54 )
+                activity_display:SetDims( "top:_; height:" .. activity_display:GetFullHeight() )
+                activity_display:Show()
+            end
+
+            if ( ( active ) and ( objective.waypoint ) ) then
+                activity_display:CreateMarker( objective.waypoint )
+            end
+        end
+
+        if ( ( pActivity.waypoint ) and ( activity_display:NumMarkers() == 0 ) ) then
+            activity_display:CreateMarker( pActivity.waypoint )
+        end
+
+        kActivityMainGroup:Show()
+    else
+        local activity_display = kActivityDisplay[ key ]
+        if ( activity_display ) then
+            activity_display:Hide()
+        end
+    end
+
+    RearrangeActivites()
+end
+
+--- Rearranges activities based on their priorityOrder
+--
+function RearrangeActivites()
+    local tbl = {}
+    for key,activity in pairs( kActivityData ) do
+        if ( kActivityDisplay[ key ] ) then
+            table.insert( tbl, { key = key, order = activity.priorityOrder } )
+        end
+    end
+
+    table.sort( tbl, function( lhs, rhs ) return lhs.order < rhs.order end )
+
+    local top = 0
+    for _,v in ipairs( tbl ) do
+        local key = v.key
+        local activity_display = kActivityDisplay[ key ]
+
+        activity_display:MoveTo( "height:_; top:" .. top, 0.1 )
+        top = top + activity_display:GetFullHeight() + 1
+    end
+end
+
+--- Removes all activities
+--
+function RemoveAllActivities()
+    for _,v in pairs( kActivityDisplay ) do
+        RemoveActivity( v:GetActivityData() )
+    end
+
+    kActivityDisplay    = {}
+    kActivityData       = {}
+    kActivityMainGroup:Hide()
+end
+
+
+--- Called when Addon is loaded
+--
+function OnComponentLoad()
+    Initialize()
+end
+
+---
+-- @param pArgs
+--
+function OnSinView( pArgs )
+    kSinView = pArgs.sinView
+    AdjustWidth()
+end
+
+---
+-- @param pArgs
+--
+function OnHudShow( pArgs )
+    kComponentFrame:ParamTo( "alpha", tonumber( pArgs.show ), pArgs.dur )
+end
+
+---
+-- @param pArgs
+--
+function OnEnterZone( pArgs )
+   kComponentFrame:Hide( Game.IsInPvP() )
+end
+
+---
+-- @param pArgs
+--
+function OnPlayerReady( pArgs )
+    OnEnterZone()
+    RunDistanceCheck()
+    OnShoppingListUpdated( pArgs )
+end
+
+---
+-- @param pArgs
+--
+function OnTrackerUpdateMission( pArgs )
+    UpdateActivity( jsontotable( pArgs.json ) )
+end
+
+---
+-- @param pArgs
+--
+function OnTrackerDeleteMission( pArgs )
+    RemoveActivity( jsontotable( pArgs.json ) )
+end
+
+---
+-- @param pArgs
+--
+function OnTrackerDelete( pArgs )
+    RemoveActivity( jsontotable( pArgs.json ) )
+end
+
+---
+-- @param pArgs
+--
+function OnTrackerUpdate( pArgs )
+    UpdateActivity( jsontotable( pArgs.json ) )
+end
+
+---
+-- @param pArgs
+--
+function OnTrackerClear( pArgs )
+    RemoveAllActivities()
+end
+
+---
+-- @param pArgs
+--
+function OnShoppingListUpdated( pArgs )
+    kShoppingList   = Player.GetShoppingList()
+    kGoalWidth      = 150 --TODO: make this tweakable?
+
+    if ( kShoppingList and kShoppingList[1] ) then
+        local item_info = Game.GetItemInfoByType( kShoppingList[1].item_id )
+        kGoalIcon:SetUrl( item_info.web_icon )
+        kShoppingItemName:SetText( item_info.name )
+
+        kGoalWidth = math.max( kGoalWidth, kShoppingItemName:GetTextDims().width )
+
+        --CreateShoppingList()
+        kGoalMainGroup:Show()
+    else
+        kShoppingList = nil
+
+        kGoalMainGroup:Hide()
+    end
+end
+
+---
+--
+function RunDistanceCheck()
+     if ( not fnDistanceCheck ) then
+        DistanceCheck()
+     end
+end
+
+---
+--
+function DistanceCheck()
+    fnDistanceCheck = nil
+
+    local player_position = Player.GetPosition()
+
+    if ( Vec3.Distance( player_position, kPlayerPosition ) > 1 ) then
+        Debug.Log( "Running Distance Check" )
+        for _,activity_display in pairs( kActivityDisplay ) do
+            if ( activity_display.mGroup ) then
+                local min_distance = 9999  --TODO: make this tweakable?
+
+                for _, tbl in ipairs( activity_display.mMarkers ) do
+                    if ( tbl.marker ) then
+                        local distance  = Vec3.Distance( player_position, tbl.waypoint )
+                        min_distance    = math.min( min_distance, distance )
+                        local alpha     = math.min( 1, math.max( 0, ( distance - skMinDistance ) / ( skMaxDistance - skMinDistance ) ) )
+
+                        tbl.marker:ParamTo( "alpha", alpha, skDistanceUpdateRate )
+                    end
+                end
+
+                if ( min_distance < 9999 ) then
+                    activity_display:SetDistance( math.floor( min_distance ) )
+                else
+                    activity_display:SetDistance( 0 )
+                end
+            end
+        end
+
+        kPlayerPosition = player_position
+    end
+
+    fnDistanceCheck = callback( DistanceCheck, nil, skDistanceUpdateRate )
+end
+
+---
+--
+function AdjustWidth()
+    local duration = 0.2 --TODO: make this tweakable?
+
+    if ( kSinView ) then
+        if ( kShoppingList ) then
+            kComponentFrame:MoveTo( "right:_; width:" .. kGoalWidth + 230, duration, 0, "ease-in" )
+        end
+    else
+        kComponentFrame:MoveTo( "right:_; width:200", duration, duration, "ease-out" )
+    end
+end
---
+++ ActivityEntry.lua
@@ -0,0 +1,233 @@
+--
+-- @Author: Pawkette
+-- @Date: 9/15/13
+--
+
+require "lib/lib_MultiArt"
+
+local skActivityTypeId  = "Activity"
+local skIconGroupId     = "IconGroup"
+local skLabelGroupId    = "LabelGroup"
+local skLabelId         = "Label"
+local skObjectiveLabelId= "ObjectiveLabel"
+local skDistanceGroupId = "DistanceGroup"
+local skObjectivesId    = "ObjectiveList"
+
+local eMission          = "Mission"
+local eEncounter        = "Encounter"
+local eAchievement      = "Achievement"
+local eOutpost          = "Outpost"
+local eResourceNode     = "ResourceNode"
+local eInstanceStep     = "InstanceStep"
+
+local bpMarker          = [=[<MapMarker name="Marker" dimensions="center-x:50%; center-y:50%; width:10; height:10" mesh="rotater_reverse" class="Marker"/>]=]
+local bpGoalEntry       = [=[<Text dimensions="left:0; right:100%; top:0; height:18" style="font:UbuntuMedium_9; valign:center"/>]=]
+
+ActivityEntry = {}
+ActivityEntry.__index = ActivityEntry
+
+setmetatable( ActivityEntry, { __call = function( cls, ... ) return cls.new( ... ) end, } )
+
+--- Creates a new Activity Entry
+-- @param pActivity the activity data table
+-- @param pUID the unique id of this entry
+--
+function ActivityEntry.new( pActivity, pUID, pParent )
+    local self = setmetatable( { }, ActivityEntry )
+    self.mActivityData  = pActivity
+    self.mUID           = pUID
+    self.mParent        = pParent
+    self.mFullHeight    = 0
+
+    self.mGroup         = Component.CreateWidget( skActivityTypeId, pParent )
+    self.mGroup:SetDims( "top:_; height:48" )
+
+    self.mIconGroup     = self.mGroup:GetChild( skIconGroupId )
+    self.mLabelGroup    = self.mIconGroup:GetChild( skLabelGroupId )
+    self.mLabel         = self.mLabelGroup:GetChild( skLabelId )
+
+    self.mMultiArt      = MultiArt.Create( self.mIconGroup:GetChild( "Icon" ) )
+    self.mDistanceGroup = self.mGroup:GetChild( skDistanceGroupId )
+    self.mDistance      = self.mDistanceGroup:GetChild( skLabelId )
+    self.mObjectives    = self.mGroup:GetChild( skObjectivesId )
+    self.mMarkers       = {}
+
+    self.mDistance:SetFont( "UbuntuBold_8" )
+    self.mLabel:SetFont( "UbuntuMedium_12" )
+
+    self:SetLabel( pActivity.title )
+
+    if ( pActivity.icon ) then
+        self.mMultiArt:SetIcon( pActivity.icon )
+
+        if ( pActivity.iconTint ) then
+            self.mMultiArt:SetParam( "tint", pActivity.iconTint )
+        end
+    elseif ( pActivity.typename == eMission ) then
+        self.mMultiArt:SetTexture( "icons", "mission_waypoint" )
+    else
+        self.mMultiArt:SetTexture( "battleframes", "unknown" )
+    end
+
+    return self
+end
+
+--- Sets this activities label
+-- @param pText
+--
+function ActivityEntry:SetLabel( pText )
+    self.mLabel:SetText( tostring( pText ) )
+
+    local dimensions = self.mLabel:GetTextDims()
+    self.mLabelGroup:SetDims( "right:_; width:" .. dimensions.width .. "; center-y:_; height:" .. dimensions.height )
+end
+
+--- Sets the distance between player and this activity
+-- @param pDistance
+--
+function ActivityEntry:SetDistance( pDistance )
+    if ( pDistance ~= 0 ) then
+        self.mDistance:SetText( tostring( Component.LookupText( "DISTANCE_IN_METERS", pDistance ) ) )
+        self.mDistanceGroup:Show()
+    else
+        self.mDistanceGroup:Hide()
+    end
+end
+
+--- GET UID
+--
+function ActivityEntry:GetUID()
+    return self.mUID
+end
+
+--- SET UID
+-- @param pUID
+--
+function ActivityEntry:SetUID( pUID )
+    self.mUID = pUID
+end
+
+--- GET Activity data
+--
+function ActivityEntry:GetActivityData()
+    return self.mActivityData
+end
+
+--- SET Activity data
+-- @param pActivity
+--
+function ActivityEntry:SetActivityData( pActivity )
+    self.mActivityData = pActivity
+end
+
+--- Get the number of markers
+--
+function ActivityEntry:NumMarkers()
+    return #self.mMarkers
+end
+
+--- Creates a map marker
+-- @param pWaypoint
+--
+function ActivityEntry:CreateMarker( pWaypoint )
+    local marker = Component.CreateWidget( bpMarker, self.mIconGroup )
+
+    marker:SetAnchorId( Player.GetTargetId() )
+    marker:SetMapping( "radial_rotate" )
+    marker:SetEdgeTracking( true )
+    marker:SetPosition( pWaypoint.x, pWaypoint.y, pWaypoint.z )
+    marker:Show()
+
+    table.insert( self.mMarkers, { marker = marker, waypoint = pWaypoint } )
+end
+
+--- Removes all markers
+--
+function ActivityEntry:RemoveAllMarkers()
+    for _,v in ipairs( self.mMarkers ) do
+       if (  v.mMarker ) then
+           Component.RemoveWidget( v.marker )
+       end
+    end
+
+    self.mMarkers = {}
+end
+
+--- Creates an Objective
+-- @param pObjective
+--
+function ActivityEntry:CreateObjective( pObjective )
+    local objective         = {}
+    objective.mWidget       = Component.CreateWidget( skObjectiveLabelId, self.mObjectives )
+
+    if ( objective.mWidget ) then
+        objective.mLabel    = objective.mWidget:GetChild( skLabelId )
+        objective.mLabel:SetFont( "UbuntuRegular_10" )
+
+        objective.mLabel:SetText( tostring( pObjective.description ) )
+
+        local dimensions    = objective.mLabel:GetTextDims()
+        objective.mWidget:SetDims( "right:_; width:" .. dimensions.width .. "; center-y:_; height:" .. dimensions.height )
+    end
+
+    return objective
+end
+
+--- Returns the number of objective widgets
+--
+function ActivityEntry:NumObjectives()
+    return self.mObjectives:GetChildCount()
+end
+
+--- Removes all objectives
+--
+function ActivityEntry:RemoveAllObjectives()
+    self:RemoveAllChildren( self.mObjectives )
+end
+
+--- Removes all children of Parent
+-- @param pParent
+--
+function ActivityEntry:RemoveAllChildren( pParent )
+    for i = pParent:GetChildCount(), 1, -1 do
+        Component.RemoveWidget( pParent:GetChild( i ) )
+    end
+end
+
+--- Sets the full height of this activity
+-- @param pHeight
+--
+function ActivityEntry:SetFullHeight( pHeight )
+    self.mFullHeight = pHeight
+end
+
+--- returns the full height of this activity
+--
+function ActivityEntry:GetFullHeight()
+    return self.mFullHeight
+end
+
+--- Wrapper
+-- @param ...
+--
+function ActivityEntry:MoveTo( ... )
+    self.mGroup:MoveTo( ... )
+end
+--- Wrapper
+--
+function ActivityEntry:Show()
+    self.mGroup:Show()
+end
+
+--- Wrapper
+--
+function ActivityEntry:Hide()
+    self.mGroup:Hide()
+end
+
+--- Wrapper
+-- @param ...
+--
+function ActivityEntry:SetDims( ... )
+    self.mGroup:SetDims( ... )
+end
